2491be137c9812c55103202fb963f6325aeae7ef
Ry Sutton <ryancesutton@gmail.com>
2020-07-09 15:14:04 -0500

    create postcomment model

---
+++ src/main/java/com/gigmatch/demo/models/PostComment.java
@@ -0,0 +1,87 @@
+package com.gigmatch.demo.models;
+
+import com.sun.xml.bind.v2.TODO;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table
+public class PostComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @CreatedDate
+    private Date createDate;
+
+    //TODO: Verify relationship for the two below
+    @ManyToOne
+    private Post post;
+
+    @OneToOne
+    private User owner;
+
+    @Column(nullable = false)
+    private String content;
+
+    public PostComment() {
+    }
+
+    public PostComment(Date createDate, Post post, User owner, String content) {
+        this.createDate = createDate;
+        this.post = post;
+        this.owner = owner;
+        this.content = content;
+    }
+
+    public PostComment(long id, Date createDate, Post post, User owner, String content) {
+        this.id = id;
+        this.createDate = createDate;
+        this.post = post;
+        this.owner = owner;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
