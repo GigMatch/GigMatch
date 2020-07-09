9271bbb9c013885a29e5490756fc18009baee429
Ry Sutton <ryancesutton@gmail.com>
2020-07-09 15:00:44 -0500

    create post model:

---
+++ src/main/java/com/gigmatch/demo/models/Post.java
@@ -0,0 +1,86 @@
+package com.gigmatch.demo.models;
+
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String img;
+
+    @Column(nullable = false)
+    private String body;
+
+    @DateTimeFormat
+    private Date dateTime;
+
+    @OneToOne
+    private User owner;
+
+    public Post() {
+    }
+
+    public Post(String img, String body, Date dateTime, User owner) {
+        this.img = img;
+        this.body = body;
+        this.dateTime = dateTime;
+        this.owner = owner;
+    }
+
+    public Post(long id, String img, String body, Date dateTime, User owner) {
+        this.id = id;
+        this.img = img;
+        this.body = body;
+        this.dateTime = dateTime;
+        this.owner = owner;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+}
