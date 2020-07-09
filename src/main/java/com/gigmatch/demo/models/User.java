085ca45b53c29a3ac47f010ac5c4a64166b96e38
Ry Sutton <ryancesutton@gmail.com>
2020-07-09 14:26:13 -0500

    create user model

---
+++ src/main/java/com/gigmatch/demo/models/User.java
@@ -0,0 +1,95 @@
+package com.gigmatch.demo.models;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 100)
+    private String first_name;
+
+    @Column(nullable = false, length = 100)
+    private String last_name;
+
+    @Column(nullable = false, length = 255, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 255, unique = true)
+    private String username;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    public User () {}
+
+    public User(String first_name, String last_name, String email, String username, String password) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(long id, String first_name, String last_name, String email, String username, String password) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
