e491e5b5db3c361516800d80da70868de38d5edf
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-07-13 08:59:04 -0500

    UserWithRoles UserController

---
+++ src/main/java/com/gigmatch/demo/services/UserDetailsLoader.java
@@ -0,0 +1,28 @@
+package com.gigmatch.demo.services;
+
+import com.gigmatch.demo.daos.UsersRepository;
+import com.gigmatch.demo.models.User;
+import com.gigmatch.demo.models.UserWithRoles;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsLoader implements UserDetailsService {
+    private final UsersRepository users;
+
+    public UserDetailsLoader(UsersRepository users) {
+        this.users = users;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = users.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("No user found for " + username);
+        }
+
+        return new UserWithRoles(user);
+    }
+}
