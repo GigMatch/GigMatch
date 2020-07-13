2f4a5308b6be542d169c06463c512f05861d5523
Esai Flores <flores.esaijonathan@gmail.com>
2020-07-13 13:34:35 -0500

    setting up to view post

--- src/main/java/com/gigmatch/demo/controllers/HomeFeedController.java
+++ src/main/java/com/gigmatch/demo/controllers/HomeFeedController.java
@@ -1,7 +1,13 @@
 package com.gigmatch.demo.controllers;
 
+import com.gigmatch.demo.models.Post;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 public class HomeFeedController {
@@ -10,4 +16,16 @@ public class HomeFeedController {
     public String feed(){
         return "homeFeed";
     }
+
+    @GetMapping("/posts/create")
+    public String viewCreateForm(Model model) {
+        model.addAttribute("post", new Post());
+        return "posts/create";
+    }
+    @PostMapping("/posts/create")
+    public String savePost(@ModelAttribute Post newPost) {
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        newPost.setOwner(currentUser);
+        Post savedPost = postsDao.save(newPost);
+        return "redirect:/posts/" + savedPost.getId();
 }
