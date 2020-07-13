7196c852b4dd5b53d93aa8c62a2d243bbe3fe2e2
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-07-13 14:44:43 -0500

    feat: closed the api token key

--- src/main/resources/static/js/keys.js
+++ src/main/resources/static/js/keys.js
@@ -1,2 +1,1 @@
-const mapboxToken = 'pk.eyJ1IjoicnlhbmNlc3V0dG9uIiwiYSI6ImNrY2ttcnE5NDAwZmoycW81ODJxc3ZhbWwifQ.H2T8PUBRpz81qRdjwPLA9g'
-
+const mapboxToken = 'pk.eyJ1IjoicnlhbmNlc3V0dG9uIiwiYSI6ImNrY2ttcnE5NDAwZmoycW81ODJxc3ZhbWwifQ.H2T8PUBRpz81qRdjwPLA9g';
