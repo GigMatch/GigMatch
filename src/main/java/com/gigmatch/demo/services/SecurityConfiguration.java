047856272d1bfb8a8867a27baa167c953e40515a
Esai Flores <flores.esaijonathan@gmail.com>
2020-07-13 21:22:43 -0500

    feat: fixed all errors and mapping

--- src/main/java/com/gigmatch/demo/services/SecurityConfiguration.java
+++ src/main/java/com/gigmatch/demo/services/SecurityConfiguration.java
@@ -48,7 +48,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 /* Login configuration */
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/") // user's home page, it can be any URL
+                .defaultSuccessUrl("/feed") // user's home page, it can be any URL
                 .permitAll() // Anyone can go to the login page
                 /* Logout configuration */
                 .and()
