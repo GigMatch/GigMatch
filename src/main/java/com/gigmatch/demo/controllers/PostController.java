d55de584ef4547848ed50ea4be18e9e1ff758bcf
Esai Flores <flores.esaijonathan@gmail.com>
2020-07-15 10:57:43 -0500

    feat: create a post for user working/ need to display it

---
+++ src/main/java/com/gigmatch/demo/controllers/PostController.java
@@ -0,0 +1,62 @@
+package com.gigmatch.demo.controllers;
+
+import com.gigmatch.demo.daos.PostsRepository;
+import com.gigmatch.demo.daos.UsersRepository;
+import com.gigmatch.demo.models.Post;
+import com.gigmatch.demo.models.User;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class PostController {
+
+    private PostsRepository postDao;
+    private UsersRepository usersDao;
+
+    public PostController(PostsRepository postsRepository, UsersRepository usersRepository){
+        this.postDao = postsRepository;
+        this.usersDao = usersRepository;
+
+    }
+
+    @GetMapping("/posts")
+//    @RequestMapping(value = "/ads", method = RequestMethod.GET)
+    public String index(Model model) {
+        List<Post> postList = postDao.findAll();
+        model.addAttribute("noPostFound", postList.size() == 0);
+        model.addAttribute("posts", postList);
+        return "posts/CreateAPost";
+
+    }
+
+//    @GetMapping("/posts/{id}")
+//    public String show(@PathVariable long id, Model model){
+//        Post ad = postDao.getOne(id);
+//        model.addAttribute("postId", id);
+//        model.addAttribute("post", ad);
+//        return "/static/homeFeed";
+//    }
+
+    @GetMapping("/posts/create")
+    public String showForm(Model viewModel){
+        viewModel.addAttribute("post", new Post());
+        return "/posts/CreateAPost";
+    }
+
+    @PostMapping("/posts/create")
+    public String save(@ModelAttribute Post postToBeSaved) {
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        postToBeSaved.setOwner(currentUser);
+        Post savedPost = postDao.save(postToBeSaved);
+        return "redirect:/feed" + savedPost.getId();
+    }
+
+}
