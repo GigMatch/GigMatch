4a3a090c8c8082a9b6ae45892144b9631330289b
Esai Flores <flores.esaijonathan@gmail.com>
2020-07-16 19:49:35 -0500

    feat: working on comment box

---
+++ src/main/java/com/gigmatch/demo/controllers/PostCommentController.java
@@ -0,0 +1,63 @@
+package com.gigmatch.demo.controllers;
+
+
+import com.gigmatch.demo.daos.PostCommentsRepository;
+import com.gigmatch.demo.daos.PostsRepository;
+import com.gigmatch.demo.daos.UsersRepository;
+import com.gigmatch.demo.models.Post;
+import com.gigmatch.demo.models.PostComment;
+import com.gigmatch.demo.models.User;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+import java.util.List;
+
+@Controller
+public class PostCommentController {
+
+    private PostCommentsRepository postCommentsDao;
+    private UsersRepository usersDao;
+    private PostsRepository postsDao;
+
+    public PostCommentController(PostCommentsRepository postCommentsRepository, UsersRepository usersRepository, PostsRepository postsRepository){
+        this.postCommentsDao = postCommentsRepository;
+        this.usersDao = usersRepository;
+        this.postsDao = postsRepository;
+    }
+
+//    @GetMapping("/feed/posts")
+//    public String indexComment(Model model) {
+//        List<PostComment> postCommentList = postCommentsDao.findAll();
+//        model.addAttribute("noCommentsFound", postCommentList.size() == 0);
+//        model.addAttribute("comments", postCommentList);
+//        return "posts/postsFeed";
+//
+//    }
+//
+//
+//    @GetMapping("/feed/posts")
+//    public String showCommentForm(Model viewModel){
+//        viewModel.addAttribute("comment", new PostComment());
+//        return "posts/postsFeed";
+//    }
+
+    @PostMapping("/feed/posts")
+    public String createComment(@ModelAttribute PostComment postCommentToBeSaved, @RequestParam(name = "postId") String postId) {
+        Post post = postsDao.getOne(Long.parseLong(postId));
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        postCommentToBeSaved.setPost(post);
+        postCommentToBeSaved.setOwner(currentUser);
+        Date currentDate = new Date();
+        postCommentToBeSaved.setCreateDate(currentDate);
+        PostComment savedComment = postCommentsDao.save(postCommentToBeSaved);
+        return "redirect:/feed/posts" + savedComment.getPost().getId();
+    }
+
+
+}
--- src/main/java/com/gigmatch/demo/daos/PostCommentsRepository.java
+++ src/main/java/com/gigmatch/demo/daos/PostCommentsRepository.java
@@ -3,5 +3,8 @@ package com.gigmatch.demo.daos;
 import com.gigmatch.demo.models.PostComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostCommentsRepository extends JpaRepository<PostComment, Long> {
+    List<PostComment> findCommentsByPostId(long id);
 }
