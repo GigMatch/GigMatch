39aa3e5a4eaffd3de74c2279b1ed6b7846cc5724
Esai Flores <flores.esaijonathan@gmail.com>
2020-07-19 14:48:08 -0500

    feat: working comments (need view)

--- src/main/java/com/gigmatch/demo/daos/PostsRepository.java
+++ src/main/java/com/gigmatch/demo/daos/PostsRepository.java
@@ -1,6 +1,7 @@
 package com.gigmatch.demo.daos;
 
 import com.gigmatch.demo.models.Post;
+import com.gigmatch.demo.models.PostComment;
 import com.gigmatch.demo.models.Profile;
 import com.gigmatch.demo.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,7 +13,6 @@ import java.util.List;
 public interface PostsRepository extends JpaRepository<Post, Long> {
     @Query("from Post as a where a.body like %:term%")
     List<Post> searchByBody(@Param("term") String term);
-
     Post findByOwner(User owner);
     List<Post> findAllByOwner(User owner);
 }
